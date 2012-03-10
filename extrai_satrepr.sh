f6a4220aebeb60592a80187ad7fa7e2e76593285
Thiago A M S Miranda <tamsmiranda@gmail.com>
2012-03-10 14:18:25 -0300

    Adicionando suporte a leitura do arquivo keywords.txt

--- extrai_satrepr.sh
+++ extrai_satrepr.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
-./gparse.sh claro tv assinar > urls.txt
-./gparse.sh claro tv assine >> urls.txt
-./gparse.sh claro tv hd assine >> urls.txt
-./gparse.sh claro tv assinatura >> urls.txt
-./gparse.sh claro tv adquira >> urls.txt
-./gparse.sh claro tv hd assine >> urls.txt
-./gparse.sh via embratel assine >> urls.txt
-./gparse.sh viaembratel assine >> urls.txt
-./gparse.sh via embratel assinar >> urls.txt
-./gparse.sh viaembratel assinar >> urls.txt
-./gparse.sh via embratel assinatura >> urls.txt
-./gparse.sh viaembratel assinatura >> urls.txt
-./gparse.sh tv satelite >> urls.txt
-./gparse.sh tv satelite assine >> urls.txt
-./gparse.sh sky assine >> urls.txt
-./gparse.sh sky fit assine >> urls.txt
-./gparse.sh sky assinar >> urls.txt
-./gparse.sh sky assinatura >> urls.txt
-./gparse.sh sky parceiro credenciado >> urls.txt
-./gparse.sh claro tv parceiro credenciado >> urls.txt
-./gparse.sh via embratel parceiro credenciado >> urls.txt
-./gparse.sh sky representante >> urls.txt
-./gparse.sh claro representante >> urls.txt
-./gparse.sh via embratel representante >> urls.txt
-cat urls.txt | awk '{gsub("http://|/.*","")}1' > domains.txt
-cat domains.txt | awk '!($0 in a) {a[$0];print}' > domains_filtered.txt
+
+# Keywords file
+KEYWORDS="./keywords.txt"
+URLS="./data/urls.txt"
+DOMAINS="./data/domains.txt"
+
+if [ ! -f "${URLS}" ]; then touch ${URLS}; fi
+
+if [ ! -f "${KEYWORDS}" ]; then
+	for KEYWORD in $@; do
+		echo -e "Obtendo urls para:${KEYWORD}\n"
+		./gparse.sh ${KEYWORD} >> ${URLS}
+	done
+else
+	cat ${KEYWORDS} | while read KEYWORD ; do
+		echo -e "Obtendo urls para:${KEYWORD}\n"
+		./gparse.sh ${KEYWORD} >> ${URLS}
+	done
+fi
+cat ${URLS} | awk '{gsub("http://|/.*","")}1' | awk '!($0 in a) {a[$0];print}' | sort > ${DOMAINS}
+
+exit 0
+
+# cat domains.txt | awk '!($0 in a) {a[$0];print}' > domains_filtered.txt
 cat domains_filtered.txt | grep embratel > domains_ok.txt
 cat domains_filtered.txt | grep claro >> domains_ok.txt
 cat domains_filtered.txt | grep sky >> domains_ok.txt
